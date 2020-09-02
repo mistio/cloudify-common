31ff07b88b000e5db757363f2508e91b7aa81e8f
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2020-09-02 17:15:24 +0200

    CY-3419 Restore fixes
    This ports #541, #542 and #543 onto 4.6.

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -1434,8 +1434,16 @@ class RemoteContextHandler(CloudifyWorkflowContextHandler):
 
     def get_operations(self, graph_id):
         client = get_rest_client()
-        operations = client.operations.list(graph_id)
-        return operations
+        ops = []
+        offset = 0
+        while True:
+            operations = client.operations.list(graph_id, _offset=offset)
+            ops += operations.items
+            if len(ops) < operations.metadata.pagination.total:
+                offset += operations.metadata.pagination.size
+            else:
+                break
+        return ops
 
     def update_operation(self, operation_id, state):
         client = get_rest_client()
